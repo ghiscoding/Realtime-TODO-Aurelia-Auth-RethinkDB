50ba81eea1235cb09c4e2295b4b423db87d35e9a
Ghislain B <gbeaulac@gmail.com>
2017-02-04 15:09:56 -0500

    Add Bootstrap tooltip & fix navbar in client-wp

--- client-wp/src/resources/index.js
+++ client-wp/src/resources/index.js
@@ -1,5 +1,6 @@
 export function configure(config) {
   config.globalResources([
+    './elements/bootstrap-tooltip',
     './elements/loading-indicator',
     './value-converters/authenticatedFilter',
     './value-converters/date-format'
