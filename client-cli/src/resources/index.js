579f58f45861d908c7a0b68b43ece61feb263d8f
Ghislain B <gbeaulac@gmail.com>
2017-03-01 23:42:14 -0500

    Remove unused client filter

--- client-cli/src/resources/index.js
+++ client-cli/src/resources/index.js
@@ -2,7 +2,6 @@ export function configure(config) {
   config.globalResources([
     './elements/bootstrap-tooltip',
     './elements/loading-indicator',
-    './value-converters/authenticatedFilter',
     './value-converters/date-format'
   ]);
 }
--- client-cli/src/resources/value-converters/authenticatedFilter.js
+++
@@ -1,7 +0,0 @@
-export class AuthenticatedFilterValueConverter {
-  toView(routes, isAuthenticated) {
-    console.log(routes);
-    if(!routes) return;
-    return routes.filter(r => r.config.auth === undefined || r.config.auth === isAuthenticated);
-  }
-}
