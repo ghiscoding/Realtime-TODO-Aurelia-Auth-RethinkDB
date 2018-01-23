803778e9d2a00b8b137ad16df8891ce5b4c76923
Ghislain B <gbeaulac@gmail.com>
2018-01-23 00:24:51 -0500

    delete and obsolete client-cli

--- client-cli/src/resources/index.js
+++
@@ -1,7 +0,0 @@
-export function configure(config) {
-  config.globalResources([
-    './elements/bootstrap-tooltip',
-    './elements/loading-indicator',
-    './value-converters/date-format'
-  ]);
-}
